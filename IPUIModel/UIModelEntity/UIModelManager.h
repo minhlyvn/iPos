a553d0f8cc217325e8b2fa2f4201f008787be18a
Minh Ly <minhlyvn@gmail.com>
2014-09-12 10:10:02 -0700

    check in

---
+++ IPUIModel/UIModelEntity/UIModelManager.h
@@ -0,0 +1,13 @@
+//
+//  UIModelManager.h
+//  IPUIModel
+//
+//  Created by cscv on 9/12/14.
+//  Copyright (c) 2014 cscv. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UIModelManager : NSObject
+
+@end
---
+++ IPUIModel/UIModelEntity/UIModelManager.m
@@ -0,0 +1,13 @@
+//
+//  UIModelManager.m
+//  IPUIModel
+//
+//  Created by cscv on 9/12/14.
+//  Copyright (c) 2014 cscv. All rights reserved.
+//
+
+#import "UIModelManager.h"
+
+@implementation UIModelManager
+
+@end
